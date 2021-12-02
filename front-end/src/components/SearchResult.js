54ea4f685d2734d7e0c58fd04aebd73c48120d08
nikolaitrifonov0 <nikolaitrifonov0@gmail.com>
2021-12-02 20:21:01 +0200

    Search results are hidden on click.

--- front-end/src/components/SearchResult.js
+++ front-end/src/components/SearchResult.js
@@ -1,15 +1,25 @@
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import styles from './SearchResult.module.css'
 
 export default function SearchResult({ id, name, artists, image }) {
+    const history = useHistory();
+
+    function goToAlbumPage(e) {
+        e.preventDefault();
+        let results = e.currentTarget.parentElement;
+        results.style.display = 'none';
+
+        history.push(`/details/${id}`);
+    }
+
     return(
-        <Link to={`/details/${id}`}>
+        <a to={`/details/${id}`} onClick={goToAlbumPage}>
             <article className={styles.result}>
                 <img src={image} className={styles.image}/>
                 <div>
                     <h3>{name}, {artists}</h3>
                 </div>
             </article>
-        </Link>
+        </a>
     );
 }
